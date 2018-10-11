6018d78e6783656e01c908792c757e5290ec8feb
Nikita Serdyukov <32428671+Lolik9524@users.noreply.github.com>
2018-10-11 11:16:42 +0700

    Add files via upload

---
+++ C#/ConsoleApplication1/ConsoleApplication1/Salary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Salary: ILeadSalary, IEmployeeSalary, IAccotreSalary
+    {
+
+       public void Pay(int amount){}
+       public int CurrentAmount { get; }
+       public int Size { get; set; }
+   }
+}
---
+++ C#/ConsoleApplication1/ConsoleApplication1/SalaryInterface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public interface ILeadSalary : ISalarySize
+    {
+        int Size { get; set; }
+    }
+
+    public interface IEmployeeSalary : ISalarySize
+    {
+       
+        int CurrentAmount { get; }
+    }
+
+    public interface IAccotreSalary : ISalarySize
+    {
+        
+        void Pay(int amount);
+
+    }
+
+    public interface ISalarySize
+    {
+        int Size { get; }
+    }
+
+}
